3be4354342a059be49277077dc32d405d605c84d
ZHANG961017 <1343507214@qq.com>
2017-10-08 15:55:29 +0800

    修改

--- Study/myshop/src/main/java/bwie/com/myshop/fragment/HomeFragment.java
+++ Study/myshop/src/main/java/bwie/com/myshop/fragment/HomeFragment.java
@@ -1,5 +1,6 @@
 package bwie.com.myshop.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,14 +21,27 @@ import bwie.com.myshop.R;
  * Created by 13435 on 2017/9/28.
  */
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
-    private ListView lv;
+    private TextView scan;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //初始化组件
+        scan = getActivity().findViewById(R.id.scan);
+        scan.setOnClickListener(this);
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_fragment, container, false);
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(getActivity(), "....", Toast.LENGTH_SHORT).show();
     }
 }
